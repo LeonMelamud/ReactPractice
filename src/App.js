b9e93641b9194dd08061316ceae646bce8bf3ec7
leon melamud <leonmelamud@gmail.com>
2020-04-26 08:44:56 +0300

    react Practice initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ErrorBoundary from './ErrorBoundary';
+import TableViewer from './TableViewer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="App">    
+    {/*<img src={logo} className="App-logo" alt="logo" />
+           <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>  
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+         >
+            Learn React
         </a>
-      </header>
-    </div>
+  */}   <ErrorBoundary>
+          <TableViewer/>
+      </ErrorBoundary>
+      <img src={logo} className="App-logo" alt="logo" />
+  </div>
   );
 }
 
